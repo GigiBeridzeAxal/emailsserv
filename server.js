4cf7f9d3d59c1b1947df238dcdf13a60be943cbf
Gigi Beridze <beridzegigi19@gmail.comecho>
2024-11-09 17:46:44 +0400

    All

--- server.js
+++ server.js
@@ -12,8 +12,9 @@ app.use(express.json())
 app.post('/' , (req,res) => {
     console.log("start Working")
 
-    const {email , number , bericht , lat , lng} = req.body
+    const {email , number , desc , lat , lng , Stockage , price , Aanvraag , region , TypeBand} = req.body
     const nodemailer = require("nodemailer");
+
 
     const locationurl = `http://localhost:3000/map/${lat}/${lng}`
 
@@ -68,22 +69,30 @@ app.post('/' , (req,res) => {
               font-weight: bold;
               text-decoration: none;
             }
+            .Mobile{
+             text-decoration:'underline';
+            }
           </style>
         </head>
         <body>
           <div class="email-container">
             <h1>Welcome to Our Service!</h1>
-            <p>Email: beridzegigi19@gmail.com,</p>
-            <p>Mobile No: 593484636</p>
-            <p>Aanvrag Type: PECHBIJSTAN LEKKE BAND</p>
-            <p>Prijs: 145€ (Bandenprijs is niet inbegrepen) </p>
-            <p>Fluweel gebroken: ZICHTBAAR SCHADE OF PLAT VERDER GEREDEN </p>
-            <p>Region: West Vlaanderen </p>
-            
-            
-            
+            <p>Email: ${email},</p>
+            <p class="Mobile" >Mobile No: ${number}</p>
+            ${Aanvraag ? `<p>Aanvrag Type: ${Aanvraag}</p>` : ''}
+            ${TypeBand ? `<p>TypeBand: ${TypeBand}</p>` : ''}
+            ${price ? `<p>Prijs: ${price} €</p>` : ''}
+  
+            ${desc ? `<p>Bericht: ${desc}</p>` : ''}
+            ${Stockage ? Stockage == 1 ?`<p>BANDEN STOCKAGE:1 SEIZOEN </p>` : `<p>BANDEN STOCKAGE:4 SEIZOEN </p>`  : ''}
+            ${region ?`<p>Region: ${region == "WestVla" ? "west-vlaanderen" : null}</p>` : ''}
 
-            <p><a href=${locationurl} class="button">Location</a></p>
+            
+            
+            
+            
+             ${lat ? `<p><a href=${locationurl} class="button">Location</a></p>` : ''}
+
           </div>
         </body>
       </html>`, // html body
